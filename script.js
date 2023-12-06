a185a192c95b001c48126ac9eae20b5c1a90a41d
DevanyCaceres <devanycaceres@gmail.com>
2023-12-05 18:54:11 -0600

    Log in form

---
+++ script.js
@@ -0,0 +1,8 @@
+function openForm() {
+    document.getElementById("Login").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("Login").style.display = "none";
+  }
+
