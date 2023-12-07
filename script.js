14f7e440c5009e277f158aadf9b4e684a49ac8e6
DevanyCaceres <devanycaceres@gmail.com>
2023-12-06 19:01:36 -0600

    inicio

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function openForm() {
+function openLogin() {
     document.getElementById("Login").style.display = "block";
   }
   
-  function closeForm() {
+  function closeLogin() {
     document.getElementById("Login").style.display = "none";
+  }
+
+  function openRegister() {
+    document.getElementById("Register").style.display = "block";
+  }
+  
+  function closeRegister() {
+    document.getElementById("Register").style.display = "none";
   }
 
